68396346befb08b9166683af90e53c84d94e3074
ToddLewden <61066835+ToddLewden@users.noreply.github.com>
2020-05-25 14:32:50 -0500

    mobile layout improving

--- sketch/js/nav.js
+++ sketch/js/nav.js
@@ -1,21 +1,16 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "100vw";
-    document.getElementById("main").style.marginLeft = "100vw";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
+    document.getElementById("open_nav").style.display = "none"
+    document.getElementById("main_container").classList.add("fade-out");
+    document.getElementById("header_container").classList.add("fade-out");
+};
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+    document.getElementById("open_nav").style.display = "inline-block";
+    document.getElementById("main_container").classList.replace("fade-out","fade-in");
+    document.getElementById("header_container").classList.replace("fade-out","fade-in");
+
+}
