e56d2bc91c67b098729a26f2bda53b55ec05bfe4
ToddLewden <61066835+ToddLewden@users.noreply.github.com>
2020-05-25 19:12:17 -0500

    refactor and added footer

--- sketch/js/nav.js
+++ sketch/js/nav.js
@@ -1,28 +1,28 @@
-function fader(element){
-    if(element.classList.contains('fade-out')){
-        element.classList.replace('fade-out','fade-in');
-    }
-    else if(element.classList.contains('fade-in')){
-        element.classList.replace('fade-in','fade-out');
-
-    }
-    else{
-        element.classList.add('fade-out');
+function fader(){
+    const to_fade = ['open_nav','main_container','header_bar','footer_bar'];
+    for(let i = 0; i<to_fade.length;i++){
+        let element = document.getElementById(to_fade[i]);
+        if(element.classList.contains('fade-out')){
+            element.classList.replace('fade-out','fade-in');
+        }
+        else if(element.classList.contains('fade-in')){
+            element.classList.replace('fade-in','fade-out');   
+        }
+        else{
+            element.classList.add('fade-out');
+        }
     }
 }
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "100vw";
-    fader(document.getElementById('open_nav'));
-    fader(document.getElementById("main_container"));
-    fader(document.getElementById("header_bar"));
+    fader()
 };
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    fader(document.getElementById('open_nav'));
-    fader(document.getElementById("main_container"));
-    fader(document.getElementById("header_bar"));
+    fader()
 }
